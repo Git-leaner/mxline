5c347679aa48d10511e805711a52ba1785f70b4e
gitLeaner <745915093@qq.com>
2018-12-13 20:40:57 +0800

    media模块  django.core.context_processors.media=>django.template.context_processors.media

--- mxline/urls.py
+++ mxline/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
     url(r'^forget/$', ForgetPwdView.as_view(), name="forget_pwd"),
     url(r'^modify_pwd/$', ModifyPwdView.as_view(), name="modify_pwd"),
     # 课程机构url配置
-    url(r'^org/', include('organization.urls', namespace="org")),
+    url(r'^org/', include(('organization.urls','org'), namespace="org")),
     #配置上传文件的访问处理函数
     url(r'^media/(?P<path>.*)$',  serve, {"document_root":MEDIA_ROOT}),
 ]
